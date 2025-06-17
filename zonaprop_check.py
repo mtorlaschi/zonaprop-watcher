8522f6a6425736dd4d2805e1a5f6fb33b4694a98
Megan Torlaschi <megantorlaschi@Megans-MacBook-Pro.local>
2025-06-17 20:28:42 -0300

    Initial commit

---
+++ zonaprop_check.py
@@ -0,0 +1,12 @@
+print("Starting script...")
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=500)
+    page = browser.new_page()
+    page.goto('https://www.zonaprop.com.ar/')
+    print(page.title())
+    browser.close()
+
+
